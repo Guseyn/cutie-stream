5308e18ac92570fcb124a0f58e48a1638028c7cc
gismayilov <gismayilov@griddynamics.com>
2018-05-24 18:57:00 +0300

    Improve ReadDataFromReadable via custom call read

--- src/readable/ReadDataFromReadable.js
+++ src/readable/ReadDataFromReadable.js
@@ -2,17 +2,18 @@
 
 const AsyncObject = require('@guseyn/cutie').AsyncObject;
 
+// Custom call to read data via 'readable' event and the readable.read() method
+const read = require('./../custom-calls/read');
+
 // Represented result is string | Buffer | null
 class ReadDataFromReadable extends AsyncObject {
 
-  constructor(stream, size) {
-    super(stream, size);
+  constructor(stream, size, limit) {
+    super(stream, size || undefined, limit || false);
   }
 
-  definedSyncCall() {
-    return (stream, size) => {
-      return stream.read(size);
-    }
+  definedAsyncCall() {
+    return read;
   }
 
 }
--- src/writable/WrittenWritable.js
+++ src/writable/WrittenWritable.js
@@ -5,12 +5,7 @@ const AsyncObject = require('@guseyn/cutie').AsyncObject;
 // Custom call to avoid memory issues using the 'drain' event
 const write = require('./../custom-calls/write');
 
-// Represented result is writable | boolean
-/*  
-  If it's boolean(always false), then the stream wishes for the calling code
-  to wait for the 'drain' event to be emitted before continuing to write additional data;
-  otherwise it's writable. 
-*/
+// Represented result is writable
 class WrittenWritable extends AsyncObject {
 
   constructor(stream, chunk, encoding) {
